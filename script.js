9d4b6982d901be20568ab8b5fd541a09d0e66168
gideonsivak <gideonsivak2007@gmail.com>
2026-06-26 03:49:37 +0530

    Finished Home Section Sucessfully

---
+++ script.js
@@ -0,0 +1,7 @@
+// நம்ம ட்வீட் பாக்ஸை தானாகவே கீழே இறக்க வைக்கும் கஸ்டம் லாஜிக்
+function autoResize(element) {
+    // முதலில் உயரத்தை ரீசெட் செய்கிறோம்
+    element.style.height = "auto";
+    // டைப் செய்த எழுத்துக்களின் உயரத்துக்கு ஏத்த மாதிரி பாக்ஸை கீழே இறக்குகிறோம்
+    element.style.height = element.scrollHeight + "px";
+}
