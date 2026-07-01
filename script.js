b46fc5fa74e90b0b3315c7075cae64a6c0091837
gideonsivak <gideonsivak2007@gmail.com>
2026-07-02 02:44:18 +0530

    final final version

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ function showSection(sectionId) {
     }
 }
 
+
+
 // நம்ம ட்வீட் பாக்ஸை தானாகவே கீழே இறக்க வைக்கும் கஸ்டம் லாஜிக்
 function autoResize(element) {
     // முதலில் உயரத்தை ரீசெட் செய்கிறோம்
@@ -28,3 +30,6 @@ function autoResize(element) {
     // டைப் செய்த எழுத்துக்களின் உயரத்துக்கு ஏத்த மாதிரி பாக்ஸை கீழே இறக்குகிறோம்
     element.style.height = element.scrollHeight + "px";
 }   
+
+
+
