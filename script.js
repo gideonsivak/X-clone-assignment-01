87f6e88b70e100b612e6dc2d202627e91775efdf
gideonsivak <gideonsivak2007@gmail.com>
2026-07-01 21:58:53 +0530

    some edits are changes

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ function autoResize(element) {
     element.style.height = "auto";
     // டைப் செய்த எழுத்துக்களின் உயரத்துக்கு ஏத்த மாதிரி பாக்ஸை கீழே இறக்குகிறோம்
     element.style.height = element.scrollHeight + "px";
-}
+}   
